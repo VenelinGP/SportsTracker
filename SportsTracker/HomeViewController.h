5f33f017836070b13795b7c980a42c97664533cf
VenelinGP <venelingp@Venelins-Mac.local>
2016-02-03 22:20:23 +0200

    Added 4 View Controllers and Views
    Added HomeViewController, DiaryViewController, ExploreViewController,
    SettingsViewController and views

---
+++ SportsTracker/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SportsTracker
+//
+//  Created by Venelin Petkov on 2/3/16.
+//  Copyright © 2016 Venelin Petkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+
+@end
+
---
+++ SportsTracker/HomeViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  SportsTracker
+//
+//  Created by Venelin Petkov on 2/3/16.
+//  Copyright © 2016 Venelin Petkov. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
